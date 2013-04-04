74af7b08e267c0be1817b90f18b1c672227646e8
Greg Joyce <greg@concretecms.com>
2013-04-04 14:54:51 -0700

    db xml and autojs

---
+++ packages/shopify/blocks/shopify_product/auto.js
@@ -0,0 +1,23 @@
+var shopifyProductBlock ={
+	init:function(){
+		this.tabSetup();
+	},
+	tabSetup: function(){
+		$('ul#ccm-blockEditPane-tabs li a').each( function(num,el){ 
+			el.onclick=function(){
+				var pane=this.id.replace('ccm-blockEditPane-tab-','');
+				productListBlock.showPane(pane);
+			}
+		});		
+	},
+	showPane:function(pane){
+		$('ul#ccm-blockEditPane-tabs li').each(function(num,el){ $(el).removeClass('ccm-nav-active') });
+		$(document.getElementById('ccm-blockEditPane-tab-'+pane).parentNode).addClass('ccm-nav-active');
+		$('div.ccm-blockEditPane').each(function(num,el){ el.style.display='none'; });
+		$('#ccm-blockEditPane-'+pane).css('display','block');
+	}
+}
+
+$(function(){
+	shopifyProductBlock.init();
+}
