06a6b4de961d458c82e229bf32488ec4f92761bb
Greg Joyce <greg@concretecms.com>
2013-04-11 11:24:10 -0700

    products actually save

--- packages/shopify/elements/product_form.php
+++ packages/shopify/elements/product_form.php
@@ -1,17 +1,17 @@
 <?defined('C5_EXECUTE') or die(_("Access Denied."));
 //var_dump($product);
 ?>
-<div class="well span6 product" product-id="<?=$product->id?>">
-	<div class="span2 scale-image" style="overflow:hidden" desired-fucking-height="140" desired-fucking-width="140">
-		<img class="img-rounded" src="<?=$product->images[0]->src?>">
+<div class="well span6 product">
+	<div class="span2 scale-image" style="overflow:hidden">
+		<img class="img-rounded" width="140" height="140" src="<?=$product->images[0]->src?>">
 	</div>
 	<div class="span3">
 		<h4><?=$product->title?></h5>
 		<p><?=strip_tags($product->body_html)?></p>
 	</div>
 	<span class="span6">
-		<button class="btn btn-danger pull-right remove-product" style="margin-right:16px"><?=t('Remove')?></button>
-		<button class="btn btn-primary pull-right add-product" style="margin-right:16px"><?=t('Choose Product')?></button>
+		<button class="btn btn-danger pull-right remove-product" ><?=t('Remove')?></button>
+		<button class="btn btn-primary pull-right add-product" product-id="<?=$product->id?>"><?=t('Choose Product')?></button>
 	</span>
 </div>
 
