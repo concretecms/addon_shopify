c417a22212ec340e42e55d2d6db41442d36d4bf6
Andrew Embler <andrew@concrete5.org>
2013-04-15 13:40:36 -0700

    making image sizing work

--- packages/shopify/blocks/shopify_product/auto.js
+++ packages/shopify/blocks/shopify_product/auto.js
@@ -2,7 +2,7 @@ var shopifyProductBlock ={
 	init:function(){
 		this.tabSetup();
 		this.productBinds();
-		//this.imageScales();
+		this.imageScales();
 	},
 	tabSetup: function(){
 		$('ul#ccm-blockEditPane-tabs li a').each( function(num,el){ 
@@ -12,6 +12,49 @@ var shopifyProductBlock ={
 			}
 		});		
 	},
+
+	imageScales: function() {
+		$('img[data-image=product]').on('load', function() {
+			$(this).attr('data-original-width', $(this).css('width').replace('px', ''));
+			$(this).attr('data-original-height', $(this).css('height').replace('px',''));
+			// now that we have the original height, we show the parent, and we add max width and max height to the image's css so it
+			// shows scaled correctly
+			$(this).css('max-width', 160).css('max-height', 160).show();
+
+			if ($.contains($(this), $('#pickedProduct'))) {
+				$('#pictureWidth').attr('data-original-width', $(this).attr('data-original-width')).attr('data-original-height', $(this).attr('data-original-height'));
+				$('#pictureHeight').attr('data-original-width', $(this).attr('data-original-width')).attr('data-original-height', $(this).attr('data-original-height'));
+
+				var $label = $('div[data-label=product-image-original] span');
+				$label.html($(this).attr('data-original-width') + 'x' + $(this).attr('data-original-height')).parent().show();
+			}
+		});
+
+		$('#pictureWidth,#pictureHeight').on('keyup', function(e) {
+			if ((e.keyCode >= 48 || e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105)) {
+				var originalWidth = parseInt($(this).attr('data-original-width'));
+				var originalHeight = parseInt($(this).attr('data-original-height'));
+				var srcWidth = parseInt($('#pictureWidth').val());
+				var srcHeight = parseInt($('#pictureHeight').val());
+			    var ratio = [srcWidth / originalWidth, srcHeight / originalHeight ];
+			    if ($(this).attr('id') == 'pictureWidth') {
+			    	var h = Math.ceil(originalHeight * ratio[0]);
+			    	if (!h) {
+			    		h = 0;
+			    	}
+				    $('#pictureHeight').val(h);
+			    } else {
+			    	var w = Math.ceil(originalWidth * ratio[1]);
+			    	if (!w) {
+			    		w = 0;
+			    	}
+				    $('#pictureWidth').val(w);
+			    }
+			}
+		});
+	},
+
+
 	/*
 	imageScales: function() {
 		//this doesn't fucking work.. The divs seem to be whatever size they want to be.
@@ -60,6 +103,13 @@ var shopifyProductBlock ={
 			$('#productID').val($(this).attr('product-id'));
 			var d = $(this).closest('.ui-dialog-content').get(0);
 			d.scrollTop = 0;
+			var im = $(this).parent().parent().find('img[data-image=product]');
+			$('#pictureWidth').attr('data-original-width', im.attr('data-original-width')).attr('data-original-height', im.attr('data-original-height')).val(im.attr('data-original-width'));
+			$('#pictureHeight').attr('data-original-width', im.attr('data-original-width')).attr('data-original-height', im.attr('data-original-height')).val(im.attr('data-original-height'));
+
+			var $label = $('div[data-label=product-image-original] span');
+			$label.html(im.attr('data-original-width') + 'x' + im.attr('data-original-height')).parent().show();
+
 			var old = $('#pickedProduct').find('div.product');
 			old.find('button.remove-product').hide();
 			old.find('button.add-product').show();
@@ -75,6 +125,8 @@ var shopifyProductBlock ={
 			$(this).parent().parent().remove();
 			$('#productID').val('');
 			$('.no-product-message').show();
+			var $label = $('div[data-label=product-image-original]');
+			$label.hide();
 			$(this).hide();
 		});
 	},
--- packages/shopify/elements/product_form.php
+++ packages/shopify/elements/product_form.php
@@ -1,24 +1,23 @@
 <?defined('C5_EXECUTE') or die(_("Access Denied."));
 //var_dump($product);
-$size = getimagesize($product->images[0]->src);
-$width = $size[0];
-$height = $size[1];
+//$size = getimagesize($product->images[0]->src);
+//$width = $size[0];
+//$height = $size[1];
 //$thumb = $ih->getThumbnail($product->images[0]->src,140,140,true);
 //rename(DIR_BASE.$thumb->src,strstr(DIR_BASE.$thumb->src,'?',true));
 //$thumb->src = strstr($thumb->src,'?',true);
 
 ?>
 <div class="well clearfix product">
-	<div class="span2 scale-image" style="overflow:hidden">
-		<img class="img-rounded" target-width="140" target-height="140" height="<?=$height?>" width="<?=$width?>" src="<?=$product->images[0]->src?>">
+	<div style="float: left; width: 160px;">
+		<img data-image="product" id="im<?=$product->id?>" src="<?=$product->images[0]->src?>" style="display: none; border: 1px solid #999; border-radius: 6px;" />
 	</div>
-	<div class="span3">
+	<div style="float: left; width: 160px; margin-left: 20px">
 		<h4><?=$product->title?></h4>
 		<p><?=strip_tags($product->body_html)?></p>
 	</div>
-	<span class="span6">
+	<div style="width: 160px; margin-left: 20px; float: left; text-align: right">
 		<button class="btn btn-danger pull-right remove-product" ><?=t('Remove')?></button>
 		<button class="btn btn-primary pull-right add-product" product-id="<?=$product->id?>"><?=t('Choose Product')?></button>
-	</span>
+	</div>
 </div>
-
